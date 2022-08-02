c329906f36cb082a1507114e22a160885c8746c3
Eqls <argnow12@gmail.com>
2022-08-02 19:53:19 +0300

    build flags fixed

--- build.zig
+++ build.zig
@@ -2,7 +2,8 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("test", null);
-    exe.addCSourceFile("src/bin/4ed_build.cpp", &[_][]const u8{"-D_GNU_SOURCE -fPIC -fpermissive -DDEV_BUILD -Isrc/custom"});
+    const flags = [_][]const u8{ "-Isrc/custom", "-D_GNU_SOURCE", "-fPIC", "-fpermissive", "-DDEV_BUILD" };
+    exe.addCSourceFile("src/bin/4ed_build.cpp", &flags);
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("c++");
 
